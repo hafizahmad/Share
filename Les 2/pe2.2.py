19d7cea18deb4cac7712f7960c8c1d5f3bed4492
Hafiz <hafiizzz@hotmail.com>
2017-09-11 15:48:53 +0200

    pe2.2

---
+++ Les 2/pe2.2.py
@@ -0,0 +1,15 @@
+hits = 12
+shields = 0
+
+if shields == 0 and hits > 10:
+    print('Je bent dood')
+    print('Je kan niet verder')
+
+result = 0
+for i in range(4):
+    result = result + i
+print(result)
+print('klaar')
+
+for i in range(11):
+    print(i)
